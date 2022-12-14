b9013b6202754472cb1d8f7a1f14080f86bfbd1d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-12-14 16:27:50 +0100

    updated sign up form

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,31 +1,89 @@
-import React from 'react'
-import { Head, Inertia, useForm } from '@inertiajs/inertia-react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { Head } from '@inertiajs/inertia-react'
+import { Inertia } from '@inertiajs/inertia'
+import { FiUser } from 'react-icons/fi'
 
 export default function Index({ name }) {
-  const { data, setData, post, processing, errors } = useForm({
+  const [formData, setFormData] = useState({
+    fullName: '',
     emailAddress: '',
     password: '',
   })
 
+  const [error, setError] = useState('')
+
+  function handleChange(e) {
+    const key = e.target.id
+    const value = e.target.value
+
+    setFormData((formData) => ({
+      ...formData,
+      [key]: value,
+    }))
+  }
+
+  function clearField() {
+    setFormData((formData) => ({
+      ...formData,
+      fullName: '',
+      emailAddress: '',
+      password: '',
+    }))
+  }
+
   function handleSubmit(e) {
     e.preventDefault()
-    post('/register')
+
+    // Password Validation
+    if (formData.password < 8) {
+      setError('Password is too short')
+    }
+
+    axios
+      .post('/register', formData)
+      .then((res) => {
+        if (res.status === 200) {
+          Inertia.get('/dashboard')
+        }
+      })
+      .catch((error) => {
+        const { message } = error.response.data
+        if (message === 'Email already in use') {
+          setError(
+            'This email address is already in use. Please log in or reset your password to continue.'
+          )
+          clearField()
+        }
+      })
   }
 
   return (
     <>
       <Head>
-        <title>{name}</title>
+        <title>QrVibe | Generate Free Dynamic QR Codes</title>
       </Head>
-      <main className="my-[8rem] flex h-full w-full items-center justify-center">
+      <main className="my-[5rem] flex h-full w-full items-center justify-center">
         <div className="w-[280px]">
-          <div>
+          <div className="w-full">
             <img src="/images/logo/qr.svg" alt="" />
+            <p className="mx-auto text-center text-[#8C8CA1]">
+              Generate Free Dynamic QR Codes
+            </p>
           </div>
-          <form
-            className="my-3 flex flex-col gap-y-4"
-            onSubmit={(e) => handleSubmit(e)}
-          >
+          <form className="my-3 flex flex-col gap-y-2" onSubmit={handleSubmit}>
+            <div className="flex h-[60px] items-center justify-start gap-x-1 rounded-md border-2 border-[#DFD9C6] p-2">
+              <FiUser className="h-[20px] w-[20px]" color="#DFD9C6" />
+              <input
+                type="text"
+                id="fullName"
+                className="h-full w-full border-0 bg-transparent text-[16px] text-secondary outline-none placeholder:text-[16px]"
+                placeholder="Full Name"
+                value={formData.fullName}
+                onChange={handleChange}
+                required
+              />
+            </div>
             <div className="flex h-[60px] items-center justify-start gap-x-1 rounded-md border-2 border-[#DFD9C6] p-2">
               <img
                 src="/images/icons/msg.svg"
@@ -34,14 +92,14 @@ export default function Index({ name }) {
               />
               <input
                 type="email"
+                id="emailAddress"
                 className="h-full w-full border-0 bg-transparent text-[16px] text-secondary outline-none placeholder:text-[16px]"
                 placeholder="Email Address"
-                value={data.emailAddress}
-                onChange={(e) => setData('emailAddress', e.target.value)}
+                value={formData.emailAddress}
+                onChange={handleChange}
                 required
               />
             </div>
-            <span>{errors.emailAddress && <div>{errors.email}</div>}</span>
             <div className="flex h-[60px] items-center justify-start gap-x-1 rounded-md border-2 border-[#DFD9C6] p-2">
               <img
                 src="/images/icons/lock.svg"
@@ -50,27 +108,29 @@ export default function Index({ name }) {
               />
               <input
                 type="password"
+                id="password"
                 className="h-full w-full border-0 bg-transparent text-[16px] text-secondary outline-none placeholder:text-[16px]"
                 placeholder="Password"
+                minLength="8"
                 required
-                value={data.password}
-                autoComplete="off"
-                onChange={(e) => setData('password', e.target.value)}
+                value={formData.password}
+                onChange={handleChange}
               />
             </div>
-            <span>{errors.password && <div>{errors.password}</div>}</span>
+            <span>
+              <div className="text-center text-error">{error}</div>
+            </span>
             <button
               type="submit"
-              disabled={processing}
-              className="h-[60px] rounded-md bg-primary text-[#ECF1F4] disabled:bg-[#f5f5f5]"
+              className="h-[60px] rounded-md bg-primary text-[#ECF1F4] disabled:outline-primary"
             >
-              Create An Account
+              Create Account
             </button>
             <p className="mt-7 text-center text-[14px] font-normal text-[#8C8CA1]">
               Already have an Account?{' '}
               <a
                 className="text-[#285A8C] underline hover:cursor-pointer"
-                href="/"
+                href="/login"
               >
                 Sign In
               </a>
