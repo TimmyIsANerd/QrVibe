f65430aa2ceeea4b1233d6bacb8f041cf8c8767e
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-12-14 17:33:21 +0100

    updated middleware

--- api/policies/isAuth.js
+++ api/policies/isAuth.js
@@ -2,6 +2,18 @@ module.exports = async function (req, res, proceed) {
   // Check if user is logged in
   const { url } = req
 
+  if (url === '/') {
+    if (req.session.userId) {
+      sails.log.info('Session Found!')
+
+      res.redirect('/dashboard')
+    } else {
+      sails.log.info('No Session Found')
+
+      res.redirect('/login')
+    }
+  }
+
   if (url.includes('/dashboard')) {
     sails.log.info('Checking for session...')
     if (!req.session.userId) {
