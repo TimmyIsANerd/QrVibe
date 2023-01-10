074affd2e9b18aa4b8e3e6015075ed5c2f948f51
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-10 06:58:21 +0100

    added custom values

--- config/custom.js
+++ config/custom.js
@@ -9,22 +9,24 @@
  */
 
 module.exports.custom = {
-
   /***************************************************************************
-  *                                                                          *
-  * Any other custom config this Sails app should use during development.    *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Any other custom config this Sails app should use during development.    *
+   *                                                                          *
+   ***************************************************************************/
   // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
 
-  verifyEmailAddresses:false,
+  passwordResetTokenTTL: 24 * 60 * 60 * 1000, // 24 hours
+  emailProofTokenTTL: 24 * 60 * 60 * 1000, // 24 hours
+
+  verifyEmailAddresses: false,
   baseUrl: 'localhost:1337',
-  smtpData:{
-    emailAddress:'adefeyitimi@gmail.com',
-    password:'F80BFC2EE15A202717F77938357C25B87CD6',
-    server:'smtp.elasticemail.com',
-    port:2525
-  }
-};
+  smtpData: {
+    emailAddress: 'adefeyitimi@gmail.com',
+    password: 'F80BFC2EE15A202717F77938357C25B87CD6',
+    server: 'smtp.elasticemail.com',
+    port: 2525,
+  },
+}
